e3ebecaeb219b1ffa1e227acddf18dcf7fdd5a6c
zhoujf <zhoujf1636@163.com>
2019-08-19 18:11:56 +0800

    hhh

--- merge_sort/merge_sort/main.cpp
+++ merge_sort/merge_sort/main.cpp
@@ -9,53 +9,87 @@
 #include <iostream>
 using namespace std;
 
-int min(int x, int y)
+//迭代法
+//int min(int x, int y)
+//{
+//    return (x < y ? x : y);
+//}
+//
+//void merge_sort(int* arr, int size)
+//{
+//    int* a = arr;
+//    int* b = (int*) malloc(size * sizeof(int));
+//    int seg,start;
+//    for(seg = 1; seg < size; seg += seg)
+//    {
+//        for(start = 0; start < size; start += seg + seg)
+//        {
+//            int low = start, mid = min(start + seg, size), high = min(start + seg + seg, size);
+//            int k = low;
+//            int start1 = low, end1 = mid;
+//            int start2 = mid, end2 = high;
+//            while(start1 < end1 && start2 < end2)
+//            {
+//                b[k++] = a[start1] < a[start2] ? a[start1++] : a[start2++];
+//            }
+//            while(start1 < end1)
+//            {
+//                b[k++] = a[start1++];
+//            }
+//            while(start2 < end2)
+//            {
+//                b[k++] = a[start2++];
+//            }
+//        }
+//        int* temp = a;
+//        a = b;
+//        b = temp;
+//    }
+//    if(a != arr)
+//    {
+//        int i;
+//        for(i = 0; i < size; i++)
+//        {
+//            b[i] = a [i];
+//        }
+//        b = a;
+//    }
+//    free(b);
+//}
+
+//递归法
+void merge_sort_recursive(int arr[], int reg[], int start, int end)
 {
-    return (x < y ? x : y);
+    if (start >= end)return;
+    int size = end - start, mid = (size >> 1) + start;
+    int start1 = start, end1 = mid;
+    int start2 = mid + 1, end2 = end;
+    merge_sort_recursive(arr, reg, start1, end1);
+    merge_sort_recursive(arr, reg, start2, end2);
+    int k = start;
+    while (start1 <= end1 && start2 <= end2)
+    {
+        reg[k++] = arr[start1] < arr[start2] ? arr[start1++] : arr[start2++];
+    }
+    while (start1 <= end1)
+    {
+        reg[k++] = arr[start1++];
+    }
+    while (start2 <= end2)
+    {
+        reg[k++] = arr[start2++];
+    }
+    for (k = start; k <= end; k++)
+    {
+        arr[k] = reg[k];
+    }
 }
 
-void merge_sort(int* arr, int size)
+void merge_sort(int arr[], const int size)
 {
-    int* a = arr;
-    int* b = (int*) malloc(size * sizeof(int));
-    int seg,start;
-    for(seg = 1; seg < size; seg += seg)
-    {
-        for(start = 0; start < size; start += seg + seg)
-        {
-            int low = start, mid = min(start + seg, size), high = min(start + seg + seg, size);
-            int k = low;
-            int start1 = low, end1 = mid;
-            int start2 = mid, end2 = high;
-            while(start1 < end1 && start2 < end2)
-            {
-                b[k++] = a[start1] < a[start2] ? a[start1++] : a[start2++];
-            }
-            while(start1 < end1)
-            {
-                b[k++] = a[start1++];
-            }
-            while(start2 < end2)
-            {
-                b[k++] = a[start2++];
-            }
-        }
-        int* temp = a;
-        a = b;
-        b = temp;
-    }
-    if(a != arr)
-    {
-        int i;
-        for(i = 0; i < size; i++)
-        {
-            b[i] = a [i];
-        }
-        b = a;
-    }
-    free(b);
+    int reg[size];
+    merge_sort_recursive(arr, reg, 0, size - 1);
 }
-
 
 int main(int argc, const char * argv[])
 {
